cd9d4ac120b0218aa0ed610907e4662cc83f9091
dmmaia <david_mmaia@hotmail.com>
2020-04-12 16:44:48 -0300

    Finishing application

--- src/app.js
+++ src/app.js
@@ -1,33 +1,125 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
+var ip = require("ip");
 
-// const { uuid } = require("uuidv4");
+let NeDb = require('nedb')
+
+let dbRepositories = new NeDb({
+  filename: 'db/repositories.db',
+  autoload: true
+})
+
+let dbLikes = new NeDb({
+  filename: 'db/likes.db',
+  autoload: true
+})
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
-
 app.get("/repositories", (request, response) => {
-  // TODO
+  dbRepositories.find({}, function (err, repositore) {
+    return response.json(repositore);
+  });
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const likes = 0;
+
+  const repositore = { title, likes, url, techs, _id: uuid() };
+
+  dbRepositories.insert(repositore, (err, repositore) => {
+    if (err) {
+      return response.status(400).send();
+    } else {
+      response.status(200).json(repositore);
+    }
+  })
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:_id", (request, response) => {
+  const { _id } = request.params;
+  const { title, likes, url, techs } = request.body;
+
+  if (likes) {
+    response.json({ "likes": 0 })
+  } else {
+
+    const newRepositore = {
+      title,
+      url,
+      techs,
+      _id,
+    };
+
+    dbRepositories.update({ _id }, newRepositore ,{}, function (err, numReplaced) {
+      if (numReplaced <= 0) {
+        response.status(400).send();
+      } else {
+        return response.json(newRepositore);
+      }
+    });
+
+  }
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:_id", (request, response) => {
+  const { _id } = request.params;
+
+  dbRepositories.remove({ _id }, {}, function (err, numRemoved) {
+    if (numRemoved <= 0) {
+      response.status(400).send();
+    } else {
+      response.status(204).send();
+    }
+  });
+
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:_id/like", (request, response) => {
+  const { _id } = request.params;
+  const repositoryId = _id;
+
+      dbLikes.find({repositoryId}, function (err, docs) {
+        const likes = docs.length+1;
+        dbRepositories.update({ _id }, { $set: { likes } }, { multi: true }, function (err, numReplaced) {
+          if (numReplaced <= 0) {
+            response.status(400).send();
+          } else {
+            dbLikes.insert({repositoryId}, (err) => {
+              if (err) {
+                response.status(400).send();
+              }else{
+                response.json({likes});
+              }
+            })
+
+          }
+        });
+      })
+});
+
+app.delete("/tests1", (request, response)=>{
+  dbRepositories.remove({ }, { multi: true }, function (err, numRemoved) {
+    db.loadDatabase(function (err) {
+      // done
+    });
+  });
+});
+
+app.delete("/tests2", (request, response)=>{
+  dbLikes.remove({ }, { multi: true }, function (err, numRemoved) {
+    db.loadDatabase(function (err) {
+      // done
+    });
+  });
 });
 
 module.exports = app;
